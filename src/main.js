9b51729e1c922fa4a994ed9dd72ab4c4e60f56d1
Loopeq <arsen.00z11@gmail.com>
2024-10-25 17:10:02 +0300

    rename domain

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import './theme.css'
 
 
 // eslint-disable-next-line
-const NON_DEV = 'https://wagers-dev.onrender.com'
+const NON_DEV = 'https://api.swaeger.com'
 // eslint-disable-next-line
 const DEV = 'http://localhost:8000'
 const app = createApp(App)
